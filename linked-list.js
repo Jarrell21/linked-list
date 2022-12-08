9dbaca797b7afa65958c0a335473338eec86ac21
Jarrell Mikko Romero <88756582+Jarrell21@users.noreply.github.com>
2022-12-08 21:09:19 +0800

    Set up project

---
+++ linked-list.js
@@ -0,0 +1,47 @@
+const Node = (value = null, nextNode = null) => {
+}
+
+const LinkedList = () => {
+    // adds a new node containing value to the end of the list
+    const append = (value) => {}
+
+    // adds a new node containing value to the start of the list
+    const prepend = (value) => {}
+
+    // returns the total number of nodes in the list
+    const size = () =>{}
+
+    // returns the first node in the list
+    const head = () => {}
+
+    // returns the last node in the list
+    const tail = () => {}
+
+    // returns the node at the given index
+    const atIndex = (index) => {}
+
+    // removes the last element from the list
+    const pop = () => {}
+
+    // returns true if the passed in value is in the list and otherwise returns false.
+    const contains = (value) => {}
+
+    // returns the index of the node containing value, or null if not found.
+    const find = (value) => {}
+
+    // represents your LinkedList objects as strings, so you can print them out and preview them in the console
+    const toString = () => {}
+
+    return {
+        append,
+        prepend,
+        size,
+        head,
+        tail,
+        atIndex,
+        pop,
+        contains,
+        find,
+        toString,
+    }
+}
